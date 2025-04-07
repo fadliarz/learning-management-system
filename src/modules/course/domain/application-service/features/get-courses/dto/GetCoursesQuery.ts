4b54073492e6a7a726401d49536cc60179142815
fadliarz <fadliarz03@gmail.com>
2025-04-07 17:07:20 +0700

    chore: update field type

--- src/modules/course/domain/application-service/features/get-courses/dto/GetCoursesQuery.ts
+++ src/modules/course/domain/application-service/features/get-courses/dto/GetCoursesQuery.ts
@@ -1,5 +1,5 @@
 import Pagination from '../../../../../../../common/common-domain/repository/Pagination';
 
 export default class GetCoursesQuery extends Pagination {
-  public categories: string[];
+  public categories: number[];
 }
