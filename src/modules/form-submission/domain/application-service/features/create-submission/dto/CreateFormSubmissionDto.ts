22612a15b8f343c374d930056c5d8b38b2c257b5
fadliarz <fadliarz03@gmail.com>
2025-04-14 21:32:02 +0700

    chore(FormSubmission): add body type for create

--- src/modules/form-submission/domain/application-service/features/create-submission/dto/CreateFormSubmissionDto.ts
+++ src/modules/form-submission/domain/application-service/features/create-submission/dto/CreateFormSubmissionDto.ts
@@ -1,8 +1,12 @@
 import ResponseItem from '../../../../domain-core/entity/ResponseItem';
 import { IsArray, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export default class CreateFormSubmissionDto {
+  @ApiProperty({
+    type: [ResponseItem],
+  })
   @IsArray({ message: 'responseItems must be an array' })
   @ValidateNested({ each: true })
   @Type(() => ResponseItem)
