bf553a64bda0baac03a56d35f21f426232ce79f8
fadliarz <fadliarz03@gmail.com>
2025-04-30 23:20:06 +0700

    doc(CourseSchedule): add mising body

--- src/modules/course-schedule/domain/application-service/features/create-schedule/dto/CreateCourseScheduleDto.ts
+++ src/modules/course-schedule/domain/application-service/features/create-schedule/dto/CreateCourseScheduleDto.ts
@@ -5,13 +5,16 @@ import {
   IsString,
   MaxLength,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export default class CreateCourseScheduleDto {
+  @ApiProperty()
   @IsString({ message: 'The title must be a string' })
   @IsNotEmpty({ message: 'The title is required' })
   @MaxLength(128, { message: 'The title must be less than 128 characters' })
   public title: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString({ message: 'The description must be a string' })
   @MaxLength(2048, {
@@ -19,17 +22,20 @@ export default class CreateCourseScheduleDto {
   })
   public description: string;
 
+  @ApiProperty()
   @IsString({ message: 'The location must be a string' })
   @IsNotEmpty({ message: 'The location is required' })
   @MaxLength(128, { message: 'The location must be less than 128 characters' })
   public location: string;
 
+  @ApiProperty()
   @IsISO8601(
     { strict: true },
     { message: 'The deadline must be a valid ISO 8601 date' },
   )
   public startDate: Date;
 
+  @ApiProperty()
   @IsISO8601(
     { strict: true },
     { message: 'The deadline must be a valid ISO 8601 date' },
