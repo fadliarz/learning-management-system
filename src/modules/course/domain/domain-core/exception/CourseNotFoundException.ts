827a8499f6e8afe444f9ee70b3a2dc48d2b24a9b
fadliarz <fadliarz03@gmail.com>
2025-05-03 19:45:27 +0700

    chore: use param as arg and add throwable field

--- src/modules/course/domain/domain-core/exception/CourseNotFoundException.ts
+++ src/modules/course/domain/domain-core/exception/CourseNotFoundException.ts
@@ -2,7 +2,11 @@ import HttpException from '../../../../../common/common-domain/exception/HttpExc
 import { HttpStatus } from '@nestjs/common';
 
 export default class CourseNotFoundException extends HttpException {
-  constructor(message: string = 'Course not found') {
-    super(HttpStatus.NOT_FOUND, message);
+  constructor(param: { message?: string; throwable?: unknown } = {}) {
+    super(
+      HttpStatus.NOT_FOUND,
+      param.message ?? 'Course not found',
+      param.throwable ?? null,
+    );
   }
 }
