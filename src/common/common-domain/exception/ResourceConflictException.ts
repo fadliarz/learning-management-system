1a8dbe90a8e4c1634b864924b03e706e4eb86a3f
fadliarz <fadliarz03@gmail.com>
2025-05-03 16:24:47 +0700

    feat: update param to be optional and update default message

--- src/common/common-domain/exception/ResourceConflictException.ts
+++ src/common/common-domain/exception/ResourceConflictException.ts
@@ -2,11 +2,11 @@ import HttpException from './HttpException';
 import { HttpStatus } from '@nestjs/common';
 
 export default class ResourceConflictException extends HttpException {
-  constructor(param: { message?: string; throwable?: unknown }) {
+  constructor(param: { message?: string; throwable?: unknown } = {}) {
     super(
       HttpStatus.CONFLICT,
       param.message ??
-        'The resource is currently being modified by another user. Please try your request again in a moment!',
+        'The resource is currently being modified by another user. Please try again in a moment!',
       param.throwable ?? null,
     );
   }
