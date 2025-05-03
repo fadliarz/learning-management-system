a73da6375146ccd37b3bc37664a59411bc9c55da
fadliarz <fadliarz03@gmail.com>
2025-05-03 20:47:25 +0700

    feat: add param as arg and add throwable field

--- src/modules/video/domain/domain-core/exception/VideoRearrangedException.ts
+++ src/modules/video/domain/domain-core/exception/VideoRearrangedException.ts
@@ -2,9 +2,11 @@ import HttpException from '../../../../../common/common-domain/exception/HttpExc
 import { HttpStatus } from '@nestjs/common';
 
 export default class VideoRearrangedException extends HttpException {
-  constructor(
-    message: string = 'Failed to update position, videos have been rearranged, please try again!',
-  ) {
-    super(HttpStatus.BAD_REQUEST, message);
+  constructor(param: { message?: string; throwable?: unknown } = {}) {
+    super(
+      HttpStatus.BAD_REQUEST,
+      param.message ?? 'Videos have been rearranged, please try again!',
+      param.throwable ?? null,
+    );
   }
 }
