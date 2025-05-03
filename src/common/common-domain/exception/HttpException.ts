5782bde8057a1af3d365cbb1c513759371c6fdb8
fadliarz <fadliarz03@gmail.com>
2025-05-03 15:47:21 +0700

    chore: update throwable type

--- src/common/common-domain/exception/HttpException.ts
+++ src/common/common-domain/exception/HttpException.ts
@@ -4,11 +4,7 @@ export default class HttpException extends DomainException {
   public statusCode: number;
   public throwable: unknown;
 
-  constructor(
-    statusCode: number,
-    message: string,
-    throwable: Error | null = null,
-  ) {
+  constructor(statusCode: number, message: string, throwable: unknown = null) {
     super(message);
     this.statusCode = statusCode;
     this.throwable = throwable;
