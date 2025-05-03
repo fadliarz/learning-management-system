ef4388249524fcce0fd9f03e55f80f9c2901d3ea
fadliarz <fadliarz03@gmail.com>
2025-05-03 16:27:50 +0700

    chore: update throwable type to be unknown

--- src/modules/category/domain/domain-core/exception/CategoryTitleAlreadyExistsException.ts
+++ src/modules/category/domain/domain-core/exception/CategoryTitleAlreadyExistsException.ts
@@ -2,7 +2,11 @@ import HttpException from '../../../../../common/common-domain/exception/HttpExc
 import { HttpStatus } from '@nestjs/common';
 
 export default class CategoryTitleAlreadyExistsException extends HttpException {
-  constructor(message: string = 'Category name already exists') {
-    super(HttpStatus.BAD_REQUEST, message);
+  constructor(param: { message?: string; throwable?: unknown } = {}) {
+    super(
+      HttpStatus.BAD_REQUEST,
+      param.message ?? 'Category name already exists',
+      param.throwable ?? null,
+    );
   }
 }
