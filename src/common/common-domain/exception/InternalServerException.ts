9baee54fb34b62c5cbfb2cc9846d8df61335a509
fadliarz <fadliarz03@gmail.com>
2025-05-03 16:27:15 +0700

    feat: update param to be optional

--- src/common/common-domain/exception/InternalServerException.ts
+++ src/common/common-domain/exception/InternalServerException.ts
@@ -2,7 +2,7 @@ import HttpException from './HttpException';
 import { HttpStatus } from '@nestjs/common';
 
 export default class InternalServerException extends HttpException {
-  constructor(param?: { message?: string; throwable?: unknown } = {}) {
+  constructor(param: { message?: string; throwable?: unknown } = {}) {
     super(
       HttpStatus.INTERNAL_SERVER_ERROR,
       param.message ?? 'Please contact your provider (Internal Server Error)!',
