1ac2f187d66acaf785caa234cb61ba5f99575ca2
fadliarz <fadliarz03@gmail.com>
2025-05-03 16:16:38 +0700

    feat: implement ResourceConflictException

---
+++ src/common/common-domain/exception/ResourceConflictException.ts
@@ -0,0 +1,13 @@
+import HttpException from './HttpException';
+import { HttpStatus } from '@nestjs/common';
+
+export default class ResourceConflictException extends HttpException {
+  constructor(param: { message?: string; throwable?: unknown }) {
+    super(
+      HttpStatus.CONFLICT,
+      param.message ??
+        'The resource is currently being modified by another user. Please try your request again in a moment!',
+      param.throwable ?? null,
+    );
+  }
+}
