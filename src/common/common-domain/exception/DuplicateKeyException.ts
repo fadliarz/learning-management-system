c4f6db47e6223e161fbc9051ff6b6c70539657d7
fadliarz <fadliarz03@gmail.com>
2025-05-03 16:16:12 +0700

    chore: update throwable type to be unknown

--- src/common/common-domain/exception/DuplicateKeyException.ts
+++ src/common/common-domain/exception/DuplicateKeyException.ts
@@ -2,7 +2,7 @@ import HttpException from './HttpException';
 import { HttpStatus } from '@nestjs/common';
 
 export default class DuplicateKeyException extends HttpException {
-  constructor(param: { message?: string; throwable?: Error }) {
+  constructor(param: { message?: string; throwable?: unknown }) {
     super(
       HttpStatus.INTERNAL_SERVER_ERROR,
       param.message ?? 'Something went wrong, please try again!',
