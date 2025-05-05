2053357d126896fce831d4107740d1a653d18c84
fadliarz <fadliarz03@gmail.com>
2025-05-05 23:28:39 +0700

    chore: update field type

--- src/config/RedisConfig.ts
+++ src/config/RedisConfig.ts
@@ -3,16 +3,16 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export default class RedisConfig {
-  private readonly _REDIS_PORT: string;
+  private readonly _REDIS_PORT: number;
   private readonly _REDIS_PASSWORD: string;
 
   constructor(private readonly _configService: ConfigService) {
-    this._REDIS_PORT = this._configService.getOrThrow<string>('REDIS_PORT');
+    this._REDIS_PORT = this._configService.getOrThrow<number>('REDIS_PORT');
     this._REDIS_PASSWORD =
       this._configService.getOrThrow<string>('REDIS_PASSWORD');
   }
 
-  get REDIS_PORT(): string {
+  get REDIS_PORT(): number {
     return this._REDIS_PORT;
   }
 
