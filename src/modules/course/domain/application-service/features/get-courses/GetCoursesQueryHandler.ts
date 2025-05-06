903ba678b06d90d2d982d296aac4c166e576e567
fadliarz <fadliarz03@gmail.com>
2025-05-06 12:08:17 +0700

    feat: add caching layer

--- src/modules/course/domain/application-service/features/get-courses/GetCoursesQueryHandler.ts
+++ src/modules/course/domain/application-service/features/get-courses/GetCoursesQueryHandler.ts
@@ -4,18 +4,21 @@ import Course from '../../../domain-core/entity/Course';
 import strictPlainToClass from '../../../../../../common/common-domain/mapper/strictPlainToClass';
 import CourseResponse from '../common/CourseResponse';
 import { DependencyInjection } from '../../../../../../common/common-domain/DependencyInjection';
-import CourseContext from '../../ports/output/context/CourseContext';
 import Pagination from '../../../../../../common/common-domain/repository/Pagination';
 import CategoryContext from '../../../../../category/domain/application-service/ports/output/context/CategoryContext';
 import Category from '../../../../../category/domain/domain-core/entity/Category';
 import CategoryResponse from '../../../../../category/domain/application-service/features/common/CategoryResponse';
-import TimerService from '../../../../../../common/common-domain/TimerService';
+import CourseHelper from '../../CourseHelper';
+import { CourseRepository } from '../../ports/output/repository/CourseRepository';
+import CourseCacheMemoryImpl from '../../../../data-access/cache/adapter/CourseCacheMemoryImpl';
 
 @Injectable()
 export default class GetCoursesQueryHandler {
   constructor(
-    @Inject(DependencyInjection.COURSE_CONTEXT)
-    private readonly courseContext: CourseContext,
+    @Inject(DependencyInjection.COURSE_CACHE_MEMORY)
+    private readonly courseCacheMemory: CourseCacheMemoryImpl,
+    @Inject(DependencyInjection.COURSE_REPOSITORY)
+    private readonly courseRepository: CourseRepository,
     @Inject(DependencyInjection.CATEGORY_CONTEXT)
     private readonly categoryContext: CategoryContext,
   ) {}
@@ -23,11 +26,31 @@ export default class GetCoursesQueryHandler {
   public async execute(
     getCoursesQuery: GetCoursesQuery,
   ): Promise<CourseResponse[]> {
-    await TimerService.sleepInMilliseconds(20);
-    const courses: Course[] = await this.courseContext.findMany({
-      ...getCoursesQuery,
-      pagination: strictPlainToClass(Pagination, getCoursesQuery),
-    });
+    const courseIds: number[] = await this.courseCacheMemory.getKeysByIndex();
+    let courses: Course[] = [];
+    for (const courseId of courseIds) {
+      let course: Course | null = await this.courseCacheMemory.get(courseId);
+      if (course) {
+        courses.push(course);
+        continue;
+      }
+      course = await this.courseRepository.findById({ courseId });
+      if (course) {
+        courses.push(course);
+        continue;
+      }
+      await this.courseCacheMemory.deleteAndRemoveIndex(courseId);
+    }
+    if (getCoursesQuery.categories) {
+      courses = CourseHelper.filterByCategories(
+        courses,
+        getCoursesQuery.categories,
+      );
+    }
+    courses = CourseHelper.paginate(
+      courses,
+      strictPlainToClass(Pagination, getCoursesQuery),
+    );
     return await this.transform(courses);
   }
 
