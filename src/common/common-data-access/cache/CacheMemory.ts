96c73bb70f8a52767e7d8019d5d82c1645b40649
fadliarz <fadliarz03@gmail.com>
2025-05-06 12:05:24 +0700

    feat: abstract cache layer

---
+++ src/common/common-data-access/cache/CacheMemory.ts
@@ -0,0 +1,23 @@
+import { CacheOptions } from './CacheOptions';
+
+export default interface CacheMemory<
+  Key extends string | number,
+  Value extends string | number | object,
+> {
+  get<T>(key: Key): Promise<Value | null>;
+
+  getKeysByIndex(index?: {}): Promise<Key[]>;
+
+  set(key: Key, value: Value, options?: CacheOptions): Promise<void>;
+
+  setAndSaveIndex(param: {
+    key: Key;
+    value: Value;
+    index?: {};
+    options?: CacheOptions;
+  }): Promise<void>;
+
+  delete(key: Key): Promise<void>;
+
+  deleteAndRemoveIndex(key: Key, index: {}): Promise<void>;
+}
