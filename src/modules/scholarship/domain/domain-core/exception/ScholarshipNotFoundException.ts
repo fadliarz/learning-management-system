26c6db3a333981d909fa6cc493768e3fec3b58a8
fadliarz <fadliarz03@gmail.com>
2025-05-07 12:11:56 +0700

    chore: update param as arg and add throwable field

--- src/modules/scholarship/domain/domain-core/exception/ScholarshipNotFoundException.ts
+++ src/modules/scholarship/domain/domain-core/exception/ScholarshipNotFoundException.ts
@@ -2,7 +2,11 @@ import HttpException from '../../../../../common/common-domain/exception/HttpExc
 import { HttpStatus } from '@nestjs/common';
 
 export default class ScholarshipNotFoundException extends HttpException {
-  constructor(message: string = 'Scholarship not found') {
-    super(HttpStatus.NOT_FOUND, message);
+  constructor(param: { message?: string; throwable?: unknown } = {}) {
+    super(
+      HttpStatus.NOT_FOUND,
+      param.message ?? 'Scholarship not found',
+      param.throwable ?? null,
+    );
   }
 }
