7cc30dc9727e3234863f82e2ad5b3931b2b9661d
fadliarz <fadliarz03@gmail.com>
2025-05-07 11:41:26 +0700

    chore: update param as arg and add throwable field

--- src/modules/attachment/domain/domain-core/exception/AttachmentNotFoundException.ts
+++ src/modules/attachment/domain/domain-core/exception/AttachmentNotFoundException.ts
@@ -2,7 +2,11 @@ import HttpException from '../../../../../common/common-domain/exception/HttpExc
 import { HttpStatus } from '@nestjs/common';
 
 export default class AttachmentNotFoundException extends HttpException {
-  constructor(message: string = 'Attachment not found') {
-    super(HttpStatus.NOT_FOUND, message);
+  constructor(param: { message?: string; throwable?: unknown } = {}) {
+    super(
+      HttpStatus.NOT_FOUND,
+      param.message ?? 'Attachment not found',
+      param.throwable ?? null,
+    );
   }
 }
