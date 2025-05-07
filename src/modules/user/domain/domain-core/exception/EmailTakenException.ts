496a8af46bac5375a7d71874f95e80db4f595ada
fadliarz <fadliarz03@gmail.com>
2025-05-07 12:20:01 +0700

    feat: update status code to conflict

--- src/modules/user/domain/domain-core/exception/EmailTakenException.ts
+++ src/modules/user/domain/domain-core/exception/EmailTakenException.ts
@@ -3,6 +3,6 @@ import { HttpStatus } from '@nestjs/common';
 
 export default class EmailTakenException extends HttpException {
   constructor(message: string = 'Email already taken, please try another one') {
-    super(HttpStatus.BAD_REQUEST, message);
+    super(HttpStatus.CONFLICT, message);
   }
 }
