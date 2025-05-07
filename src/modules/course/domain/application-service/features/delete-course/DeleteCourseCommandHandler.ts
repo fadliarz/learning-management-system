60e2500492f751cefb449c39e69a8c8cc1595507
fadliarz <fadliarz03@gmail.com>
2025-05-07 10:11:20 +0700

    feat: invalidate the cache after deleting the record

--- src/modules/course/domain/application-service/features/delete-course/DeleteCourseCommandHandler.ts
+++ src/modules/course/domain/application-service/features/delete-course/DeleteCourseCommandHandler.ts
@@ -3,12 +3,15 @@ import AuthorizationService from '../../../../../../common/common-domain/feature
 import { CourseRepository } from '../../ports/output/repository/CourseRepository';
 import { Inject } from '@nestjs/common';
 import { DependencyInjection } from '../../../../../../common/common-domain/DependencyInjection';
+import CourseCacheMemoryImpl from '../../../../data-access/cache/adapter/CourseCacheMemoryImpl';
 
 export default class DeleteCourseCommandHandler {
   constructor(
     private readonly authorizationService: AuthorizationService,
     @Inject(DependencyInjection.COURSE_REPOSITORY)
     private readonly courseRepository: CourseRepository,
+    @Inject(DependencyInjection.COURSE_CACHE_MEMORY)
+    private readonly courseCacheMemory: CourseCacheMemoryImpl,
   ) {}
 
   public async execute(
@@ -20,5 +23,8 @@ export default class DeleteCourseCommandHandler {
     await this.courseRepository.deleteIfExistsOrThrow({
       ...deleteCourseCommand,
     });
+    await this.courseCacheMemory.deleteAndRemoveIndex(
+      deleteCourseCommand.courseId,
+    );
   }
 }
