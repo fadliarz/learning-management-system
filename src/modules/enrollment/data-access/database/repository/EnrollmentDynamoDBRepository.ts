edd5ef6a6a83cd6af07a690c8082003b71030fc5
fadliarz <fadliarz03@gmail.com>
2025-05-07 12:02:05 +0700

    feat: implement idempotent delete

--- src/modules/enrollment/data-access/database/repository/EnrollmentDynamoDBRepository.ts
+++ src/modules/enrollment/data-access/database/repository/EnrollmentDynamoDBRepository.ts
@@ -1,23 +1,18 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { DependencyInjection } from '../../../../../common/common-domain/DependencyInjection';
 import {
-  DeleteCommand,
   DynamoDBDocumentClient,
   QueryCommand,
   TransactWriteCommand,
 } from '@aws-sdk/lib-dynamodb';
 import DynamoDBConfig from '../../../../../config/DynamoDBConfig';
-import {
-  ConditionalCheckFailedException,
-  TransactionCanceledException,
-} from '@aws-sdk/client-dynamodb';
+import { TransactionCanceledException } from '@aws-sdk/client-dynamodb';
 import EnrollmentEntity from '../entity/EnrollmentEntity';
 import CourseKey from '../../../../course/data-access/database/entity/CourseKey';
 import EnrollmentKey from '../entity/EnrollmentKey';
 import { DynamoDBExceptionCode } from '../../../../../common/common-domain/DynamoDBExceptionCode';
 import EnrollmentAlreadyExistsException from '../../../domain/domain-core/exception/EnrollmentAlreadyExistsException';
 import CourseNotFoundException from '../../../../course/domain/domain-core/exception/CourseNotFoundException';
-import EnrollmentNotFoundException from '../../../domain/domain-core/exception/EnrollmentNotFoundException';
 import Pagination from '../../../../../common/common-domain/repository/Pagination';
 import strictPlainToClass from '../../../../../common/common-domain/mapper/strictPlainToClass';
 import InternalServerException from '../../../../../common/common-domain/exception/InternalServerException';
@@ -175,34 +170,11 @@ export default class EnrollmentDynamoDBRepository {
           throw new InternalServerException({ throwable: exception });
         if (
           CancellationReasons[0].Code ===
-          DynamoDBExceptionCode.CONDITIONAL_CHECK_FAILED
-        )
-          throw new EnrollmentNotFoundException({ throwable: exception });
-        if (
+            DynamoDBExceptionCode.CONDITIONAL_CHECK_FAILED ||
           CancellationReasons[1].Code ===
-          DynamoDBExceptionCode.CONDITIONAL_CHECK_FAILED
-        ) {
-          try {
-            await this.dynamoDBDocumentClient.send(
-              new DeleteCommand({
-                TableName: this.dynamoDBConfig.ENROLLMENT_TABLE,
-                Key: new EnrollmentKey({ userId, classId }),
-                ExpressionAttributeNames: {
-                  '#courseId': 'courseId',
-                },
-                ExpressionAttributeValues: {
-                  ':value0': courseId,
-                },
-                ConditionExpression:
-                  'attribute_exists(userId) AND attribute_exists(classId) AND #courseId = :value0',
-              }),
-            );
-          } catch (exception) {
-            if (exception instanceof ConditionalCheckFailedException) {
-              throw new EnrollmentNotFoundException({ throwable: exception });
-            }
-          }
-        }
+            DynamoDBExceptionCode.CONDITIONAL_CHECK_FAILED
+        )
+          return;
       }
       throw new InternalServerException({ throwable: exception });
     }
