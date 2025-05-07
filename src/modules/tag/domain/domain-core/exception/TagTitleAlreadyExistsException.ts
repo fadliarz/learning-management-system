ea87ab4fd6f200ff2c36a155d54d84bf7a351ce1
fadliarz <fadliarz03@gmail.com>
2025-05-07 12:13:46 +0700

    chore: update param as arg and add throwable field

--- src/modules/tag/domain/domain-core/exception/TagTitleAlreadyExistsException.ts
+++ src/modules/tag/domain/domain-core/exception/TagTitleAlreadyExistsException.ts
@@ -2,7 +2,11 @@ import HttpException from '../../../../../common/common-domain/exception/HttpExc
 import { HttpStatus } from '@nestjs/common';
 
 export default class TagTitleAlreadyExistsException extends HttpException {
-  constructor(message: string = 'Tag name already exists') {
-    super(HttpStatus.BAD_REQUEST, message);
+  constructor(param: { message?: string; throwable?: unknown } = {}) {
+    super(
+      HttpStatus.BAD_REQUEST,
+      param.message ?? 'Tag name already exists',
+      param.throwable ?? null,
+    );
   }
 }
