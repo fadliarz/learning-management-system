dbd02a0f190a9aae2f969412190dabdbec85a6a8
fadliarz <fadliarz03@gmail.com>
2025-05-07 12:23:23 +0700

    chore: update param as arg and add throwable field

--- src/modules/user-assignment/domain/domain-core/exception/UserAssignmentNotFoundException.ts
+++ src/modules/user-assignment/domain/domain-core/exception/UserAssignmentNotFoundException.ts
@@ -2,7 +2,11 @@ import HttpException from '../../../../../common/common-domain/exception/HttpExc
 import { HttpStatus } from '@nestjs/common';
 
 export default class UserAssignmentNotFoundException extends HttpException {
-  constructor(message: string = 'User assignment not found') {
-    super(HttpStatus.NOT_FOUND, message);
+  constructor(param: { message?: string; throwable?: unknown } = {}) {
+    super(
+      HttpStatus.NOT_FOUND,
+      param.message ?? 'User assignment not found',
+      param.throwable ?? null,
+    );
   }
 }
