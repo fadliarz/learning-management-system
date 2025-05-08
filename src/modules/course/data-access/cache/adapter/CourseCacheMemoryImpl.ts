83945f0cd33ba7a495972b20f9bd07b7d20a63c6
fadliarz <fadliarz03@gmail.com>
2025-05-08 07:40:26 +0700

    feat: implement automatic keys revalidation with ttl

--- src/modules/course/data-access/cache/adapter/CourseCacheMemoryImpl.ts
+++ src/modules/course/data-access/cache/adapter/CourseCacheMemoryImpl.ts
@@ -5,6 +5,8 @@ import { Inject, Injectable } from '@nestjs/common';
 import CourseRedisCacheMemory from '../memory/CourseRedisCacheMemory';
 import { DependencyInjection } from '../../../../../common/common-domain/DependencyInjection';
 import CacheConfig from '../../../../../config/CacheConfig';
+import { CourseRepository } from '../../../domain/application-service/ports/output/repository/CourseRepository';
+import Pagination from '../../../../../common/common-domain/repository/Pagination';
 
 @Injectable()
 export default class CourseCacheMemoryImpl implements CourseCacheMemory {
@@ -14,6 +16,8 @@ export default class CourseCacheMemoryImpl implements CourseCacheMemory {
     @Inject(DependencyInjection.COURSE_REDIS_CACHE_MEMORY)
     private readonly courseRedisCacheMemory: CourseRedisCacheMemory,
     private readonly cacheConfig: CacheConfig,
+    @Inject(DependencyInjection.COURSE_REPOSITORY)
+    private readonly courseRepository: CourseRepository,
   ) {}
 
   public async get(key: number): Promise<Course | null> {
@@ -21,8 +25,14 @@ export default class CourseCacheMemoryImpl implements CourseCacheMemory {
   }
 
   public async getKeysByIndex(): Promise<number[]> {
-    const keys = await this.courseRedisCacheMemory.getKeysByIndex(
+    let keys: string[];
+    keys = await this.courseRedisCacheMemory.getKeysByIndex(this.INDEX_NAME);
+    if (keys.length === 0) {
+      keys = await this.loadKeysByIndex();
+    }
+    await this.courseRedisCacheMemory.setExpiresIfNotSet(
       this.INDEX_NAME,
+      this.cacheConfig.DEFAULT_INDEX_TTL_IN_SEC,
     );
     return keys.map((key) => this.revertKey(key));
   }
@@ -72,5 +82,22 @@ export default class CourseCacheMemoryImpl implements CourseCacheMemory {
 
   private revertKey(key: string): number {
     return parseInt(key.replace(`${this.INDEX_NAME}#`, ''));
+  }
+
+  private async loadKeysByIndex(): Promise<string[]> {
+    const courses: Course[] = await this.courseRepository.findMany({
+      pagination: new Pagination(),
+    });
+    for (const course of courses) {
+      await this.courseRedisCacheMemory.setAndSaveIndex({
+        key: this.transformKey(course.courseId),
+        value: course,
+        options: {
+          ttl: this.cacheConfig.DEFAULT_TTL_IN_SEC,
+        },
+        index: this.INDEX_NAME,
+      });
+    }
+    return await this.courseRedisCacheMemory.getKeysByIndex(this.INDEX_NAME);
   }
 }
