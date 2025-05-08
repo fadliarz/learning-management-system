4d2b1171d6bc6ce91790caa5082cf64213cdb304
fadliarz <fadliarz03@gmail.com>
2025-05-08 07:39:53 +0700

    feat: add DEFAULT_INDEX_TTL_IN_SEC field

--- src/config/CacheConfig.ts
+++ src/config/CacheConfig.ts
@@ -4,13 +4,21 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export default class CacheConfig {
   private readonly _DEFAULT_TTL_IN_SEC: number;
+  private readonly _DEFAULT_INDEX_TTL_IN_SEC: number;
 
   constructor(private readonly _configService: ConfigService) {
     this._DEFAULT_TTL_IN_SEC =
       this._configService.getOrThrow<number>('DEFAULT_TTL_IN_SEC');
+    this._DEFAULT_INDEX_TTL_IN_SEC = this._configService.getOrThrow<number>(
+      'DEFAULT_INDEX_TTL_IN_SEC',
+    );
   }
 
   get DEFAULT_TTL_IN_SEC(): number {
     return this._DEFAULT_TTL_IN_SEC;
+  }
+
+  get DEFAULT_INDEX_TTL_IN_SEC(): number {
+    return this._DEFAULT_INDEX_TTL_IN_SEC;
   }
 }
