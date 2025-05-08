54c75a90f7a01136f31ce286cc12f4934a2847a2
fadliarz <fadliarz03@gmail.com>
2025-05-08 12:43:43 +0700

    feat: add cache layer before hitting the database

--- src/modules/category/domain/application-service/features/get-category/GetCategoryQueryHandler.ts
+++ src/modules/category/domain/application-service/features/get-category/GetCategoryQueryHandler.ts
@@ -4,10 +4,14 @@ import GetCategoryQuery from './dto/GetCategoryQuery';
 import CategoryResponse from '../common/CategoryResponse';
 import strictPlainToClass from '../../../../../../common/common-domain/mapper/strictPlainToClass';
 import { DependencyInjection } from '../../../../../../common/common-domain/DependencyInjection';
+import CategoryCacheMemoryImpl from '../../../../data-access/cache/adapter/CategoryCacheMemoryImpl';
+import Category from '../../../domain-core/entity/Category';
 
 @Injectable()
 export default class GetCategoryQueryHandler {
   constructor(
+    @Inject(DependencyInjection.CATEGORY_CACHE_MEMORY)
+    private readonly categoryCacheMemory: CategoryCacheMemoryImpl,
     @Inject(DependencyInjection.CATEGORY_REPOSITORY)
     private readonly categoryRepository: CategoryRepository,
   ) {}
@@ -15,8 +19,17 @@ export default class GetCategoryQueryHandler {
   public async execute(
     getCategoryQuery: GetCategoryQuery,
   ): Promise<CategoryResponse> {
+    const cachedCategory: Category | null = await this.categoryCacheMemory.get(
+      getCategoryQuery.categoryId,
+    );
+    if (cachedCategory)
+      return strictPlainToClass(CategoryResponse, cachedCategory);
     const category = await this.categoryRepository.findByIdOrThrow({
       ...getCategoryQuery,
+    });
+    await this.categoryCacheMemory.setAndSaveIndex({
+      key: getCategoryQuery.categoryId,
+      value: category,
     });
     return strictPlainToClass(CategoryResponse, category);
   }
