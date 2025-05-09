6ba7787e79ee5da0aa92173310b5b56eb3a3dc2d
fadliarz <fadliarz03@gmail.com>
2025-05-09 21:20:09 +0700

    feat: add cache layer before hitting the database

--- src/modules/scholarship/domain/application-service/features/get-scholarship/GetScholarshipQueryHandler.ts
+++ src/modules/scholarship/domain/application-service/features/get-scholarship/GetScholarshipQueryHandler.ts
@@ -8,10 +8,13 @@ import { DependencyInjection } from '../../../../../../common/common-domain/Depe
 import Tag from '../../../../../tag/domain/domain-core/entity/Tag';
 import TagResponse from '../../../../../tag/domain/application-service/features/common/TagResponse';
 import TagContext from '../../../../../tag/domain/application-service/ports/output/context/TagContext';
+import ScholarshipCacheMemoryImpl from '../../../../data-access/cache/adapter/ScholarshipCacheMemoryImpl';
 
 @Injectable()
 export default class GetScholarshipQueryHandler {
   constructor(
+    @Inject(DependencyInjection.SCHOLARSHIP_CACHE_MEMORY)
+    private readonly scholarshipCacheMemory: ScholarshipCacheMemoryImpl,
     @Inject(DependencyInjection.SCHOLARSHIP_REPOSITORY)
     private readonly scholarshipRepository: ScholarshipRepository,
     @Inject(DependencyInjection.TAG_CONTEXT)
@@ -21,10 +24,19 @@ export default class GetScholarshipQueryHandler {
   public async execute(
     getScholarshipQuery: GetScholarshipQuery,
   ): Promise<ScholarshipResponse> {
+    const cachedScholarship: Scholarship | null =
+      await this.scholarshipCacheMemory.get({
+        scholarshipId: getScholarshipQuery.scholarshipId,
+      });
+    if (cachedScholarship) return this.transform(cachedScholarship);
     const scholarship: Scholarship =
       await this.scholarshipRepository.findByIdOrThrow({
         ...getScholarshipQuery,
       });
+    await this.scholarshipCacheMemory.setAndSaveIndex({
+      key: { scholarshipId: getScholarshipQuery.scholarshipId },
+      value: scholarship,
+    });
     return await this.transform(scholarship);
   }
 
