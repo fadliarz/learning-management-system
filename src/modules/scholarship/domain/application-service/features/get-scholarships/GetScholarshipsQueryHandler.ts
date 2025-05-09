ea331fa23a7a8def30def0b71ebcd3121cfa57c7
fadliarz <fadliarz03@gmail.com>
2025-05-09 21:10:05 +0700

    feat: add cache layer before hitting the database

--- src/modules/scholarship/domain/application-service/features/get-scholarships/GetScholarshipsQueryHandler.ts
+++ src/modules/scholarship/domain/application-service/features/get-scholarships/GetScholarshipsQueryHandler.ts
@@ -3,29 +3,56 @@ import GetScholarshipsQuery from './dto/GetScholarshipsQuery';
 import ScholarshipResponse from '../common/ScholarshipResponse';
 import Scholarship from '../../../domain-core/entity/Scholarship';
 import strictPlainToClass from '../../../../../../common/common-domain/mapper/strictPlainToClass';
-import Pagination from '../../../../../../common/common-domain/repository/Pagination';
 import { DependencyInjection } from '../../../../../../common/common-domain/DependencyInjection';
-import ScholarshipContext from '../../ports/output/context/ScholarshipContext';
 import TagResponse from '../../../../../tag/domain/application-service/features/common/TagResponse';
 import TagContext from '../../../../../tag/domain/application-service/ports/output/context/TagContext';
 import Tag from '../../../../../tag/domain/domain-core/entity/Tag';
+import ScholarshipCacheMemoryImpl from '../../../../data-access/cache/adapter/ScholarshipCacheMemoryImpl';
+import { ScholarshipRepository } from '../../ports/output/repository/ScholarshipRepository';
+import Pagination from '../../../../../../common/common-domain/repository/Pagination';
+import ScholarshipHelper from '../../ScholarshipHelper';
 
 @Injectable()
 export default class GetScholarshipsQueryHandler {
   constructor(
-    @Inject(DependencyInjection.SCHOLARSHIP_CONTEXT)
-    private readonly scholarshipContext: ScholarshipContext,
     @Inject(DependencyInjection.TAG_CONTEXT)
     private readonly tagContext: TagContext,
+    @Inject(DependencyInjection.SCHOLARSHIP_REPOSITORY)
+    private readonly scholarshipRepository: ScholarshipRepository,
+    @Inject(DependencyInjection.SCHOLARSHIP_CACHE_MEMORY)
+    private readonly scholarshipCacheMemory: ScholarshipCacheMemoryImpl,
   ) {}
 
   public async execute(
     getScholarshipsQuery: GetScholarshipsQuery,
   ): Promise<ScholarshipResponse[]> {
-    const scholarships: Scholarship[] = await this.scholarshipContext.findMany({
-      ...getScholarshipsQuery,
-      pagination: strictPlainToClass(Pagination, getScholarshipsQuery),
-    });
+    const resourceIds: { scholarshipId: number }[] =
+      await this.scholarshipCacheMemory.getKeysByIndex({});
+    let scholarships: Scholarship[] = [];
+    for (const resourceId of resourceIds) {
+      let scholarship: Scholarship | null =
+        await this.scholarshipCacheMemory.get(resourceId);
+      if (scholarship) {
+        scholarships.push(scholarship);
+        continue;
+      }
+      scholarship = await this.scholarshipRepository.findById(resourceId);
+      if (scholarship) {
+        scholarships.push(scholarship);
+        continue;
+      }
+      await this.scholarshipCacheMemory.deleteAndRemoveIndex(resourceId, {});
+    }
+    if (getScholarshipsQuery.tags) {
+      scholarships = ScholarshipHelper.filterByTags(
+        scholarships,
+        getScholarshipsQuery.tags,
+      );
+    }
+    scholarships = ScholarshipHelper.paginate(
+      scholarships,
+      strictPlainToClass(Pagination, getScholarshipsQuery),
+    );
     return await this.transform(scholarships);
   }
 
