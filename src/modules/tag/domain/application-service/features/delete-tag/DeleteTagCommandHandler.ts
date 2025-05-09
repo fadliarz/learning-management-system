735b37afa5ab057d300768af4853bd7576238de1
fadliarz <fadliarz03@gmail.com>
2025-05-09 22:01:20 +0700

    feat: invalidate the cache after deleting the record

--- src/modules/tag/domain/application-service/features/delete-tag/DeleteTagCommandHandler.ts
+++ src/modules/tag/domain/application-service/features/delete-tag/DeleteTagCommandHandler.ts
@@ -3,6 +3,7 @@ import { TagRepository } from '../../ports/output/repository/TagRepository';
 import AuthorizationService from '../../../../../../common/common-domain/features/AuthorizationService';
 import DeleteTagCommand from './dto/DeleteTagCommand';
 import { DependencyInjection } from '../../../../../../common/common-domain/DependencyInjection';
+import TagCacheMemoryImpl from '../../../../data-access/cache/adapter/TagCacheMemoryImpl';
 
 @Injectable()
 export default class DeleteTagCommandHandler {
@@ -10,6 +11,8 @@ export default class DeleteTagCommandHandler {
     private readonly authorizationService: AuthorizationService,
     @Inject(DependencyInjection.TAG_REPOSITORY)
     private readonly tagRepository: TagRepository,
+    @Inject(DependencyInjection.TAG_CACHE_MEMORY)
+    private readonly tagCacheMemory: TagCacheMemoryImpl,
   ) {}
 
   public async execute(deleteTagCommand: DeleteTagCommand): Promise<void> {
@@ -19,5 +22,11 @@ export default class DeleteTagCommandHandler {
     await this.tagRepository.deleteIfExistsOrThrow({
       ...deleteTagCommand,
     });
+    await this.tagCacheMemory.deleteAndRemoveIndex(
+      {
+        tagId: deleteTagCommand.tagId,
+      },
+      {},
+    );
   }
 }
