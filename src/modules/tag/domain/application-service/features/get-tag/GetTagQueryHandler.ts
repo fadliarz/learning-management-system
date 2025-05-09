2e1fee0bf52b3178bea3bb1b88814d1a72674fb8
fadliarz <fadliarz03@gmail.com>
2025-05-09 22:00:36 +0700

    feat: add cache layer before hitting the database

--- src/modules/tag/domain/application-service/features/get-tag/GetTagQueryHandler.ts
+++ src/modules/tag/domain/application-service/features/get-tag/GetTagQueryHandler.ts
@@ -4,17 +4,29 @@ import GetTagQuery from './dto/GetTagQuery';
 import TagResponse from '../common/TagResponse';
 import strictPlainToClass from '../../../../../../common/common-domain/mapper/strictPlainToClass';
 import { DependencyInjection } from '../../../../../../common/common-domain/DependencyInjection';
+import TagCacheMemoryImpl from '../../../../data-access/cache/adapter/TagCacheMemoryImpl';
+import Tag from '../../../domain-core/entity/Tag';
 
 @Injectable()
 export default class GetTagQueryHandler {
   constructor(
+    @Inject(DependencyInjection.TAG_CACHE_MEMORY)
+    private readonly tagCacheMemory: TagCacheMemoryImpl,
     @Inject(DependencyInjection.TAG_REPOSITORY)
     private readonly tagRepository: TagRepository,
   ) {}
 
   public async execute(getTagQuery: GetTagQuery): Promise<TagResponse> {
+    const cachedTag: Tag | null = await this.tagCacheMemory.get({
+      tagId: getTagQuery.tagId,
+    });
+    if (cachedTag) return strictPlainToClass(TagResponse, cachedTag);
     const tag = await this.tagRepository.findByIdOrThrow({
       ...getTagQuery,
+    });
+    await this.tagCacheMemory.setAndSaveIndex({
+      key: { tagId: getTagQuery.tagId },
+      value: tag,
     });
     return strictPlainToClass(TagResponse, tag);
   }
